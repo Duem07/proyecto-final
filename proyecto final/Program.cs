a1dacb50704da1c3d9aef7180970cf079be4bf57
Duem07 <Familia Ortega@LAPTOP-QOR5U70Q>
2022-11-05 20:32:52 -0600

    Agregar archivos de proyecto.

---
+++ proyecto final/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool año;
+
+            Console.WriteLine("Ingrese un año: ");
+            año = Console.ReadLine();
+
+
+
+        }
+    }
+}

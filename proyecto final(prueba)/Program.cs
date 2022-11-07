ce81742d9aab2ff42de11d1d11b0548fcb913248
Duem07 <Familia Ortega@LAPTOP-QOR5U70Q>
2022-11-07 12:24:30 -0600

    .

---
+++ proyecto final(prueba)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_final_prueba_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int año;
+            Console.WriteLine("Ingrese un año: ");
+            año = int.Parse(Console.ReadLine());
+
+        if(año % 4 == 0)
+        {
+
+            
+            if (año % 100 != 0 || año % 400 == 0)
+            {
+                Console.WriteLine("El año {0} si es bisiesto", año);
+            }
+
+            else
+            {
+                Console.WriteLine("El año {0} no es bisiesto ", año);
+            }
+           }
+            else
+          {
+            Console.WriteLine("El año {0} no es bisiesto ", año);
+           }
+                Console.ReadKey();
+        }
+       }
+}
--- proyecto final/Program.cs
+++ proyecto final/Program.cs
@@ -4,19 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+Duilio Ortega
+05/11/2022
+Programa que muestra si un año es bisiesto o no
+*/
 namespace proyecto_final
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            bool año;
-
+            int año;
             Console.WriteLine("Ingrese un año: ");
-            año = Console.ReadLine();
+            año = int.Parse(Console.ReadLine()); //Le pide al usuario que ingrese el año
+            {
+                if (año % 4 == 0 && año % 100 != 0 || año % 400 == 0)
+                {
+                    Console.WriteLine("El año {0} si es bisiesto", año);
+                } //Es una condicion para que diga que el año debe ser divisor entre 4, entre 100 y entre 400
 
+                else
+                {
+                    Console.WriteLine("El año {0} no es bisiesto ", año);
+                } //dice que si no es ninguna de las demas que muestre que no es bisiesto 
 
+                Console.ReadKey();
 
+            }
         }
     }
 }
